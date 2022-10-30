654ffed682fe48406786d9d3ffe24ebdc59ad9f8
Tahlil <bsse0803@iit.du.ac.bd>
2022-10-30 19:23:02 +0600

    Add data structure for supply chain protocol contract

--- contracts/SCProtocol.sol
+++ contracts/SCProtocol.sol
@@ -5,5 +5,76 @@ pragma solidity ^0.8.17;
 // import "hardhat/console.sol";
 
 contract SCProtocol {
+    mapping(string=>address) admins;
+    enum WorkerType{
+        Person,
+        IOT
+    }
+    struct Worker{
+    string name;
+    WorkerType workerType;
+    string email;
+    string contact;
+    }
+    uint256 latestWorkerID;
+    mapping(uint256=>Worker) workers;
+
+    struct Location{
+    string district;
+    string subDistrict;
+    string details;
+    }
+
+    bytes[] allLocationHashes;
+
+    mapping(bytes=>Location) locations;
+
+    struct Product{
+        uint256 productId;
+        string productName;
+        uint256 productPrice;
+        string description;
+        string unit;
+        uint256 amount; 
+    } 
+
+    Product[] products;
+    enum LocationType{
+        Production,
+        Warehouse,
+        Retail
+    }
+
+    enum Status{
+        Pending,
+        Complete,
+        Damaged,
+        Details
+    }
+
+    struct SupplyChainStage{
+        uint256 workerID;
+        LocationType locationType;
+        bytes locationHash;
+        uint256 expectedPickUpdate;  
+        uint256 price;
+        Status status; 
+        string statusDetails;
+    }
+
+    uint256 latestProjectId;
+
+    struct Project{
+        string projectName;
+        uint256 startDate;
+        uint256 endDate;
+        uint256 productId;
+        uint256 quantity;
+        string unit;
+        SupplyChainStage[] supplyChainStages;
+    }
+    // project ID -> array of Project
+    mapping(uint256=>Project) projects; 
+
 
 }
