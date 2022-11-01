a2eaef31d423f0f4e23ed1f492ab78e4c78ae831
sihan-tawsik <sihantawsik@gmail.com>
2022-11-01 18:55:24 +0600

    add: Address seed

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,14 +1,39 @@
 import FireFly from "@hyperledger/firefly-sdk";
 import { randCity, randText, randAddress, Address } from "@ngneat/falso";
+import { PromisePool } from "@supercharge/promise-pool";
 
 const firefly = new FireFly({ host: "http://localhost:5000" });
 
-const seedAddress = async (count: number) => {
+const invokeFirefly = async (
+  inputs: { [key: string]: any }[],
+  func: string
+) => {
+  const { results, errors } = await PromisePool.withConcurrency(5)
+    .for(inputs)
+    .process(async (input: { [key: string]: any }) => {
+      return await firefly.invokeContractAPI("SCProtocol", func, {
+        input,
+      });
+    });
+  return { results, errors };
+};
+
+const seedAddress = async (count: number, email: string) => {
   const districts = randCity({ length: count });
   const subDistricts = randAddress({ length: count }).map(
     (addr: Address) => addr.county
   );
   const details = randText({ length: count });
+  const inputs = [];
+  for (let i = 0; i < count; i++) {
+    inputs.push({
+      district: districts[i],
+      subdistrict: subDistricts[i],
+      details: details[i],
+      email: email,
+    });
+  }
+  return await invokeFirefly(inputs, "addLocation");
 };
 
 (async () => {
@@ -33,4 +58,7 @@ const seedAddress = async (count: number) => {
   if (!isAdmin) {
     throw new Error("Email not admin");
   }
+
+  const resAddress = await seedAddress(10, email);
+  console.log("Addresses seeded:", resAddress.results.length);
 })();
